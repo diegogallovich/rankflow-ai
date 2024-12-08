24cf59eadd61d468e0e63bbea812429d6bff8bf7
Diego Gallovich <diego@Mac.home>
2024-12-08 15:42:17 +0100

    abstracted constants to environment file and whitelisted env file in gitignore

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
-export const APPWRITE_URL = "https://cloud.appwrite.io/v1";
-export const APPWRITE_PROJECT = "6697687d002cbd31ba6b";
-export const APPWRITE_JWT_KEY = "appwrite-jwt";
+export const APPWRITE_URL = process.env.APPWRITE_URL;
+export const APPWRITE_PROJECT = process.env.APPWRITE_PROJECT;
+export const APPWRITE_JWT_KEY = process.env.APPWRITE_JWT_KEY;
+
